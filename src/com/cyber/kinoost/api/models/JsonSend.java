ab0984a79e12d8076f25822f5abb946932622897
Leschuk <Leschuk@10.3.3>
2014-03-25 00:53:26 +0400

    My task

---
+++ src/com/cyber/kinoost/api/models/JsonSend.java
@@ -0,0 +1,28 @@
+package com.cyber.kinoost.api.models;
+
+
+import java.util.List;
+import com.cyber.kinoost.db.models.*;
+
+public class JsonSend {
+    private List<Favorites> favorites;
+    private List<MusicRating> musicrating;
+    public List<Favorites> getFavorites() {
+		return favorites;
+	}
+	public void setFavorites(List<Favorites> favorites) {
+		this.favorites = favorites;
+	}
+	public List<MusicRating> getmMusicRating() {
+		return musicrating;
+	}
+	public void setMusicRating(List<MusicRating> MusicRating) {
+		this.musicrating = MusicRating;
+	}
+	
+	@Override
+	public String toString() {
+		return "JsonUpdate [favorites=" + favorites + ", musicrating=" + musicrating
+				+ "]";
+	}
+}
--- src/com/cyber/kinoost/api/tasks/HttpAsyncTaskUpdate.java
+++ src/com/cyber/kinoost/api/tasks/HttpAsyncTaskUpdate.java
@@ -1,23 +1,32 @@
 package com.cyber.kinoost.api.tasks;
 
+import java.sql.SQLException;
+import java.util.Date;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.cyber.kinoost.api.*;
+
 import java.io.IOException;
 
 import com.cyber.kinoost.api.models.JsonUpdate;
+import com.cyber.kinoost.api.models.JsonSend;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
 
-import com.cyber.kinoost.api.*;
 import com.cyber.kinoost.db.repositories.FilmRepository;
+import com.cyber.kinoost.db.repositories.FavoritesRepository;
+import com.cyber.kinoost.db.repositories.MusicRatingRepository;
 
 public class HttpAsyncTaskUpdate extends AsyncTask<String, Void, String> {
 	private Context context;
-	
+	SharedPreferences prefs;
 	public HttpAsyncTaskUpdate(Context context) {
 		super();
 		this.context = context;
@@ -26,6 +35,24 @@ public class HttpAsyncTaskUpdate extends AsyncTask<String, Void, String> {
 	private Context getContext() {
 		return this.context;
 	}
+	public void SentToServer(){
+		String URL = "https://www.microsoft.com"; //windows все проверяет так (:
+		ApiHelper API = new ApiHelper();
+		JsonSend json = new JsonSend();
+		FavoritesRepository Favor = new FavoritesRepository(getContext());
+		MusicRatingRepository MusRate = new MusicRatingRepository(getContext());
+	    Date storedDate = new Date(prefs.getLong("com.cyber.kinoost.update.datetime", 0));
+		try{
+		json.setFavorites(Favor.getFavorites(0, 0));
+		json.setMusicRating(MusRate.getMusicRatingByDate(storedDate, 0, 0));
+		String SendString = json.toString();
+		@SuppressWarnings("static-access")
+		String Log = API.POST(URL, SendString);
+		System.out.println(Log);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		}
 	
     @Override
     protected String doInBackground(String... params) {
