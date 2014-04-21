fa31fcb5006089f6d64c9a369bb742ff543885c3
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-04-22 03:21:28 +0400

    merge:Dyachenko

--- src/com/cyber/kinoost/adapters/ListAdapter.java
+++ src/com/cyber/kinoost/adapters/ListAdapter.java
@@ -36,19 +36,16 @@ public class ListAdapter extends BaseAdapter {
 		    Display display = context.getWindowManager().getDefaultDisplay();
 		  }
 
-		  // кол-во элементов
 		  @Override
 		  public int getCount() {
 		    return filmTuples.size();
 		  }
 
-		  // элемент по позиции
 		  @Override
 		  public Object getItem(int position) {
 		    return filmTuples.get(position);
 		  }
 
-		  // id по позиции
 		  @Override
 		  public long getItemId(int position) {
 		    return position;
@@ -58,48 +55,21 @@ public class ListAdapter extends BaseAdapter {
 			  return (Tuple<Film, Film>) getItem(position);
 		  }
 
-		  // пункт списка
-		  @Override
+		  @SuppressWarnings("deprecation")
+		@Override
 		  public View getView(int position, View convertView, ViewGroup parent) {
-		    // используем созданные, но не используемые view
+		    
 		    View view = convertView;
 		    if (view == null) {
 		      view = lInflater.inflate(R.layout.item, parent, false);
 		    }
 
-		    // заполняем View в пункте списка данными из товаров: наименование, цена
-		    // и картинка
-	/*	    ((TextView) view.findViewById(R.id.text_name_l)).setText(p.name);
-		    ((TextView) view.findViewById(R.id.tvPrice)).setText(p.price + "");
-		    ((ImageView) view.findViewById(R.id.ivImage)).setImageResource(p.image);
-
-		    CheckBox cbBuy = (CheckBox) view.findViewById(R.id.cbBox);
-		    // присваиваем чекбоксу обработчик
-		    cbBuy.setOnCheckedChangeListener(myCheckChangList);
-		    // пишем позицию
-		    cbBuy.setTag(position);
-		    // заполняем данными из товаров: в корзине или нет
-		    cbBuy.setChecked(p.box);*/
-
 		    RelativeLayout rightTable = (RelativeLayout) view.findViewById(R.id.table_l);
 		    RelativeLayout leftTable = (RelativeLayout) view.findViewById(R.id.table_r);
 
 		    Display display = context.getWindowManager().getDefaultDisplay();
-		    int width = display.getWidth(); //да, я знаю что устарело, но так удобнее
-			int height = display.getHeight(); 
-////		    Display display = getWindowManager().getDefaultDisplay(); 
-////		    Display display =  ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-////		    Point size = new Point();
-////		    display.getSize(size);
-//////		    DisplayMetrics metrics = new DisplayMetrics();
-//       // WindowManager windowManager = (WindowManager) Activity.getContext().getSystemService(Activity.WINDOW_SERVICE);
-////		    DisplayMetrics metrics = new DisplayMetrics();
-////		    getWindowManager().getDefaultDisplay().getMetrics(metrics);
-//		    
-//		    
-//		    
-//		    
-//		    
+			int width = display.getWidth();
+			int height = display.getHeight(); 	    
 
 		    rightTable.getLayoutParams().height = Math.round(width/2) - 30;
 		    rightTable.getLayoutParams().width = Math.round(width/2) - 30;
@@ -112,7 +82,6 @@ public class ListAdapter extends BaseAdapter {
 
 		    Tuple<Film, Film> item = getFilm(position);
 		    
-		    // подгрузим картинки
 		    final Film fst = item.getFst();
 		    final Film snd = item.getSnd();
 		    
@@ -122,8 +91,6 @@ public class ListAdapter extends BaseAdapter {
 		    if(snd != null)
 		    	imageLoader.DisplayImage(snd.getImgUrl(), imgr);
 		    
-		    Log.d("ListAdapter", item.toString());
-		    
 		    leftTable.setOnClickListener(new OnClickListener() {
 		    	@Override 
 		    	public void onClick(View arg0) {
@@ -131,9 +98,7 @@ public class ListAdapter extends BaseAdapter {
 		    		intent.setClass(context, FilmActivity.class);
 		    		intent.putExtra("img", fst.getImgUrl());
 		    		intent.putExtra("film", fst);
-		    		Log.v("OLOLO", fst.getImgUrl());
 		    		intent.putExtra("name", fst.getName());
-		    		Log.v("OLOLO", fst.getName());
 		    		context.startActivity(intent);
 		    	}
 		    });
@@ -145,9 +110,7 @@ public class ListAdapter extends BaseAdapter {
 		    		intent.setClass(context, FilmActivity.class);
 		    		intent.putExtra("img", snd.getImgUrl());
 		    		intent.putExtra("film", snd);
-		    		Log.v("OLOLO", snd.getImgUrl());
 		    		intent.putExtra("name", snd.getName());
-		    		Log.v("OLOLO1", fst.getName());
 		    		context.startActivity(intent);
 		    	}
 		    });
