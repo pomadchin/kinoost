bb7707fb9e06248531806b21d506e29dd7c8077d
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-05-12 12:18:03 +0400

    fix PersistUserRunnable

--- src/com/cyber/kinoost/api/tasks/PersistUserRunnable.java
+++ src/com/cyber/kinoost/api/tasks/PersistUserRunnable.java
@@ -1,7 +1,6 @@
 package com.cyber.kinoost.api.tasks;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
@@ -27,21 +26,17 @@ public class PersistUserRunnable implements Runnable {
 		if ((int) account.getUserId() <= 0)
 			return;
 		
-		Collection<Long> usersList = new ArrayList<Long>();
+		ArrayList<Long> usersList = new ArrayList<Long>();
 		com.cyber.kinoost.api.vk.sources.User vkUser = null;
-		ArrayList<com.cyber.kinoost.api.vk.sources.User> userList = null;
 		
 		Api api = new Api(account);
-		
 		usersList.add(new Long(account.getUserId()));
 
 		try {
-			userList = api.getProfiles(usersList, null, null, null, null, null);
-			if(userList.size() > 0)
-				vkUser = userList.get(0);
-			if (vkUser != null) {
+			vkUser = api.getProfiles(usersList, null, null, null, null, null).get(0);
+			if (vkUser != null) 
 				account.setName(vkUser.last_name + " " + vkUser.first_name);
-			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
